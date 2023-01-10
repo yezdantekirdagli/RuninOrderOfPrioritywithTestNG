f8c325b5b0e3c0e3841eb20df09ee61d0d51d431
Yezdan Tekirdagli <yezdan.tekirdagli@gmail.com>
2023-01-10 18:20:18 +0300

    Add files via upload

---
+++ TestNG.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+import java.security.acl.Group;
+
+public class TestNG {
+    @Test(priority = 1)
+    public void test1() {
+        System.out.println("test1");
+    }
+    @Test(priority = 2)
+    public void test2() {
+        System.out.println("test2");
+    }
+    @Test
+    public void test3() {
+        System.out.println("test3");
+    }
+    @Test(priority = 3)
+    public void test4() {
+        System.out.println("test4");
+    }
+
+    }
+
